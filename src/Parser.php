30c56ff47022f611da2da9e05ac9f543b8b1a624
Spencer Mortensen <smortensen@dattobackup.com>
2015-04-10 18:05:07 -0400

    Updated the project name

--- src/Parser.php
+++ src/Parser.php
@@ -3,19 +3,19 @@
 /**
  * Copyright (C) 2015 Datto, Inc.
  *
- * This file is part of PHP JSON-RPC.
+ * This file is part of PHP Parser.
  *
- * PHP JSON-RPC is free software: you can redistribute it and/or modify
+ * PHP Parser is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License, version 3,
  * as published by the Free Software Foundation.
  *
- * PHP JSON-RPC is distributed in the hope that it will be useful,
+ * PHP Parser is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with PHP JSON-RPC. If not, see <http://www.gnu.org/licenses/>.
+ * along with PHP Parser. If not, see <http://www.gnu.org/licenses/>.
  *
  * @author Spencer Mortensen <smortensen@datto.com>
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL-3.0
